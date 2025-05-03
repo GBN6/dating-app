c6fad79e15d4b72c3c8f1dadac834ccd05993253
GBN6 <grzegorzbandyra@gmail.com>
2025-05-03 14:03:52 +0200

    feat: initial create angular app, small clean up

--- api/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03850cd287492de58f969e57a69a07afde5ca1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f7ebad87f35e904fffc65b8b4070e558e01d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
