5314e1770755cbff69f50d7d2b9b839d91a4bec0
GBN6 <grzegorzbandyra@gmail.com>
2025-05-03 15:30:37 +0200

    feat: add styles, typo, colors, mixins

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'client';
+  private readonly httpClient = inject(HttpClient);
+  public users: Observable<any[]> = this.httpClient.get<any[]>('https://localhost:5001/api/users');
+
+  // ngOnInit() {
+  //   this.httpClient.get('https://localhost:5001/api/users').subscribe({
+  //     next: () => {},
+  //     error: (error) => console.log('Error:', error),
+  //     complete: () => console.log('request complete'),
+  //   });
+  // }
 }
