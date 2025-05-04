6ca3cbda140427f609266d16bdbc72f3c1c51eff
GBN6 <grzegorzbandyra@gmail.com>
2025-05-04 14:49:24 +0200

    feat: add buttons to navbar, styling improvments

--- client/src/app/core/navbar/navbar.component.ts
+++ client/src/app/core/navbar/navbar.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IconComponent } from '../../shared/components/icons/icon.component';
 import { NAVBAR_CONFIG } from './navbar.const';
 import { LinkComponent } from '../../shared/components/link/link.component';
+import { ButtonComponent } from "../../shared/components/button/button.component";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.scss'],
-  imports: [IconComponent, LinkComponent],
+  imports: [IconComponent, LinkComponent, ButtonComponent],
 })
 export class NavbarComponent {
   public readonly navbarConfig = NAVBAR_CONFIG;
---
+++ client/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,29 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  input,
+  Output,
+} from '@angular/core';
+import { Design, Type } from './button.model';
+import { NgTemplateOutlet } from '@angular/common';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-button',
+  templateUrl: 'button.component.html',
+  styleUrls: ['button.component.scss'],
+  imports: [NgTemplateOutlet],
+})
+export class ButtonComponent {
+  public design = input<Design>('primary');
+  public disabled = input(false);
+  public type = input<Type>('button');
+  public isDisabledOnRequest = input(false);
+
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  public handleClick() {
+    this.buttonClick.emit();
+  }
+}
