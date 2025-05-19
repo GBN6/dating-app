dbe533afe2e06a6284423c5516accb3fbc0899c9
GBN6 <grzegorzbandyra@gmail.com>
2025-05-19 20:30:57 +0200

    refactor: change styling to adjust mobile view

--- client/src/app/modules/members/member-details/member-details.component.ts
+++ client/src/app/modules/members/member-details/member-details.component.ts
@@ -3,14 +3,14 @@ import { MembersService } from '../members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../members.model';
 import { Observable, switchMap, tap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
 	selector: 'app-member-details',
-	imports: [AsyncPipe, MatCardModule, ButtonComponent, MatTabsModule],
+	imports: [AsyncPipe, MatCardModule, ButtonComponent, MatTabsModule, DatePipe],
 	templateUrl: './member-details.component.html',
 	styleUrl: './member-details.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
