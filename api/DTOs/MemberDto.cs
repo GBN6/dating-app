a25539a1f6243ac0467db593fb79c29cf353ac44
GBN6 <grzegorzbandyra@gmail.com>
2025-05-20 19:14:50 +0200

    refactor: new migration fixing column name

--- api/DTOs/MemberDto.cs
+++ api/DTOs/MemberDto.cs
@@ -14,7 +14,7 @@ public class MemberDto
     public DateTime Created { get; set; }
     public DateTime LastActive { get; set; }
     public string? Gender { get; set; }
-    public string? Intorudction { get; set; }
+    public string? Introduction { get; set; }
     public string? Interests { get; set; }
     public string? LookingFor { get; set; }
     public string? City { get; set; }
