0c4d46c3bb04db12740b568fcfbb75094c94a0d3
GBN6 <grzegorzbandyra@gmail.com>
2025-06-01 10:32:00 +0200

    feat: add new user likes table

--- api/Data/DataContex.cs
+++ api/Data/DataContex.cs
@@ -1,9 +1,21 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace API.Data;
 
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<AppUser> Users { get; set; } = null!;
+    public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<UserLike>().HasKey(key => new { key.SourceUserId, key.TargetUserId });
+
+        builder.Entity<UserLike>().HasOne(source => source.SourceUser).WithMany(like => like.LikedUsers).HasForeignKey(source => source.SourceUserId).OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<UserLike>().HasOne(source => source.TargetUser).WithMany(like => like.LikedByUsers).HasForeignKey(source => source.TargetUserId).OnDelete(DeleteBehavior.Cascade);
+    }
 }
